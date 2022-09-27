4fcee7e8e1434b333c44dc318aea19a4fa14f8c0
15Carmen <carmen.martin@iesnervion.es>
2022-09-27 10:54:31 +0200

    commit1

--- src/ejbasico1/EjBasico1.java
+++ src/ejbasico1/EjBasico1.java
@@ -5,16 +5,20 @@ import java.util.Scanner;
 public class EjBasico1 {
     public static void main(String[] args) {
 
-        //Creo el Scanner para leer por teclado
+        //Creamos el scanner para poder leer lo que el ususario introduzca por consola
         Scanner sc = new Scanner (System.in);
 
-        //Le pedimos el número al usuario
+        //Declaramos la variable num
+        int num;
+
+        /**
+         * Le pedimos al usuario que introduzca un número entero y lo guardamos en la
+         *  variable antes declarada
+         */
         System.out.println("Introduzca un número entero:");
+        num=sc.nextInt();
 
-        //Declaro la variable num donde se guarda el número introducido y lo guardo
-        int num=sc.nextInt();
-
-        //Muestro el número introducido por consola
+        //Mostramos la variable por consola
         System.out.println("El número es:"+num);
     }
 }
